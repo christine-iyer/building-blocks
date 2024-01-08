6d3fa76875b659bbf415d30c53f15e055f7c1990
chrisIyer <hellohotbox420@gmail.com>
2024-01-08 14:14:30 -0500

    working on read ReadMore

--- src/App.js
+++ src/App.js
@@ -3,16 +3,7 @@ import NewBlock from "./components/NewBlock";
 import './App.css';
 
 function App() {
-  const [maxBid, setMaxBid] = useState({
-    title: "",
-  });
-  const handleChange = (event) => {
-    setMaxBid({ ...maxBid, [event.target.name]: event.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDafault();
-  };
+  
   return (
     <div className="App">
       <h2>Start editing to see some magic happen!</h2>
---
+++ src/components/ReadMore.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import Button from "./Button";
+
+export default function ReadMore(){
+     return(
+          <>
+          </>
+     )
+}
+
+
